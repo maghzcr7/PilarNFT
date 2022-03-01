4adcc24336e683135d9de053e1a34f00dca4a013
maghzcr7 <99786378+maghzcr7@users.noreply.github.com>
2022-03-01 13:32:38 +0330

    Delete secret.js

--- secret.js
+++
@@ -1,7 +0,0 @@
-const privateKeys = ['04aad7ec4c25a4bbbe740f624892810508bd59581bcd38db3aa6417b6fcad0fb'];
-const infuraURL = ['https://rinkeby.infura.io/v3/4eef91774fbf46ceb899626b8bfbdeea'];
-const infuraEthURL = ['https://mainnet.infura.io/v3/4eef91774fbf46ceb899626b8bfbdeea'];
-
-module.exports = {
-    privateKeys
-}
